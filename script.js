bfea48e43263eedb2a59c8e1c42aea6abeee72a6
Yaniv Izaki <Yaniv Izaki>
2016-11-08 09:43:49 +0200

    .

--- script.js
+++ script.js
@@ -7,16 +7,14 @@ function addNew() {
 	var task = taskElem.value;
 	//alert(task);
 
-	var table = document.getElementById('myTable');
+	var mytable = document.getElementById('mytable');
 	var row = mytable.insertRow(0);
 	var cell1 = row.insertCell(0);
 	var cell2 = row.insertCell(1);
 
 	var x = document.createElement('INPUT');
 	x.setAttribute("type","checkbox");
-	x.setAttribute("id","chk");
-
 
 	cell1.innerHTML = task;
-	cell2.innerHTML = document.getElementById('chk');
+	cell2.appendChild(x);
 }
